30d6634129daa36cffe662fceffa66ef1f3ca2ba
Sylwester Madej <sylwek.madej@gmail.com>
2017-08-30 09:56:17 +0200

    Place picker

--- app/src/main/java/com/soldiersofmobile/atmlocator/AddAtmActivity.kt
+++ app/src/main/java/com/soldiersofmobile/atmlocator/AddAtmActivity.kt
@@ -2,14 +2,15 @@ package com.soldiersofmobile.atmlocator
 
 import android.app.Activity
 import android.app.Fragment
+import android.content.Intent
 import android.database.Observable
-import android.databinding.DataBindingUtil
-import android.databinding.ObservableDouble
-import android.databinding.ObservableField
+import android.databinding.*
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.google.android.gms.location.places.Place
+import com.google.android.gms.location.places.ui.PlacePicker
 import com.soldiersofmobile.atmlocator.databinding.ActivityAddAtmBinding
 
 class AddAtmActivity : AppCompatActivity() {
@@ -25,13 +26,38 @@ class AddAtmActivity : AppCompatActivity() {
         binding.presenter = AddAtmPresenter(this, binding.model)
 
     }
+
+    fun pickPlace() {
+        val intent = PlacePicker.IntentBuilder().build(this)
+        startActivityForResult(intent, AddAtmActivity.REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val place = PlacePicker.getPlace(this, data)
+            toast("Picked:$place")
+
+        }
+    }
+
+    companion object {
+        val REQUEST_CODE = 1
+    }
 }
 
 fun Activity.toast(message: String) {
     Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
 }
 
 class AddAtmPresenter(val activity: AddAtmActivity, val model: AddAtmModel) {
+
+    fun pick(view: View) {
+        activity.pickPlace()
+    }
+
     fun save(view: View) {
         activity.toast("Save clicked: $model")
     }
@@ -41,14 +67,33 @@ class AddAtmPresenter(val activity: AddAtmActivity, val model: AddAtmModel) {
             address.set("")
             lat.set(0.0)
             lng.set(0.0)
+            note = "notefromcode"
         }
     }
 }
 
-data class AddAtmModel(var lat: ObservableDouble, var lng: ObservableDouble, var address: ObservableField<String>) {
-//    val latAsString: String
-//        get() = lat.get().toString()
-//    val lngAsString: String
-//        get() = lng.get().toString()
+data class AddAtmModel(var lat: ObservableDouble,
+                       var lng: ObservableDouble,
+                       var address: ObservableField<String>) : BaseObservable() {
+
+    @Bindable
+    var hasNote: Boolean = true
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.hasNote)
+            if (!value) {
+                note = ""
+            }
+        }
+    @Bindable
+    var note: String = "note"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.note)
+            if (value != "") {
+                hasNote = true
+            }
+        }
+
 }
 
